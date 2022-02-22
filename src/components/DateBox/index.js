887e24604407086676d58abeff93dba9eae6b079
rheemcode <oluwabless123@gmail.com>
2022-02-22 15:39:20 +0100

    Fx Bug

--- src/components/DateBox/index.js
+++ src/components/DateBox/index.js
@@ -3,23 +3,23 @@ import DatePicker from 'react-date-picker';
 import moment from "moment"
 import './style.scss';
 
-const Textbox = ({ label, startDate, formatMonth, onChange,defaultValue, value, boxClasses, error, iconUrl, onIconClick, disabled, min, required, max }) => {
+const Textbox = ({ label, startDate, formatMonth, onChange, defaultValue, value, boxClasses, error, iconUrl, onIconClick, disabled, min, required, max }) => {
 
-  return(
+  return (
     <div className={`${boxClasses} date-box`} tabIndex="1">
       <fieldset className={`${error && 'error'} ${disabled && 'disabled-input'}`}>
         {/* <legend id="label-legend" className={` pl-2 pr-2 ${error && 'label-error'}`}>{label}</legend> */}
         <div className="d-flex position-relative">
-        { label ? <span className={`label position-absolute ${boxClasses?.includes("active") ? "active" : ""} ${error && 'label-error'}`}>{label}</span> : "" }
-          <DatePicker activeStartDate={startDate} defaultValue={defaultValue} disabled={disabled} onChange={onChange} value={value} maxDate={max} minDate={min} required={required} format="dd/MM/yyyy" formatMonth={formatMonth} clearIcon={null} />
-          {error && 
-            <img src={error && require("#/assets/icons/error-icon.svg")} alt="textbox-error"/>
+          {label ? <span className={`label position-absolute ${boxClasses?.includes("active") ? "active" : ""} ${error && 'label-error'}`}>{label}</span> : ""}
+          <DatePicker defaultActiveStartDate={startDate} defaultValue={defaultValue} disabled={disabled} onChange={onChange} value={value} maxDate={max} minDate={min} required={required} format="dd/MM/yyyy" formatMonth={formatMonth} clearIcon={null} />
+          {error &&
+            <img src={error && require("#/assets/icons/error-icon.svg")} alt="textbox-error" />
           }
           {
-            iconUrl && <img src={iconUrl} alt="textbox-icon" onClick={onIconClick}/>
+            iconUrl && <img src={iconUrl} alt="textbox-icon" onClick={onIconClick} />
           }
         </div>
-        
+
       </fieldset>
       <p className={`${error ? 'd-block' : 'd-none'} input-error mt-1`}>{error}</p>
     </div>
